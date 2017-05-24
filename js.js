cf58daeb72a9873231960acb5087c0cb409d6b29
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-23 17:09:50 -0700

    added random quote

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
 //Random Quote Machine
+var loadQuote = $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+  var quote = "" + json.quoteText + ""
+  $(".quote").html(quote)
+  var author = "~" + json.quoteAuthor + "";
+  $(".author").html(author)
+  })
+
+
+
+
+  /*$('.newQuote').on('click', function() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+    $(".quote").html(JSON.stringify(json));
+    })
+  })*/
+
