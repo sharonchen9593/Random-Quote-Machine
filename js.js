58633bb02744ae9302d964f4d3423123eef2a146
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-23 17:11:17 -0700

    New quote button works now

--- js.js
+++ js.js
@@ -9,6 +9,21 @@ var loadQuote = $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&la
 
 
 
+
+$(document).ready(function() {
+  loadQuote;
+
+  $('.newQuote').on('click', function() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+  var quote = "" + json.quoteText + ""
+  $(".quote").html(quote)
+  var author = "~" + json.quoteAuthor + "";
+  $(".author").html(author)
+  })
+  })
+})
+
+
   /*$('.newQuote').on('click', function() {
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
     $(".quote").html(JSON.stringify(json));
