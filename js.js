8365af255fd16674170944914e3288c8860a371a
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-23 17:37:30 -0700

    changed font and added color

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 //Random Quote Machine
 var loadQuote = $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
-  var quote = "" + json.quoteText + ""
+  var quote = '' + json.quoteText + ''
   $(".quote").html(quote)
   var author = "~" + json.quoteAuthor + "";
   $(".author").html(author)
@@ -15,18 +15,11 @@ $(document).ready(function() {
 
   $('.newQuote').on('click', function() {
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
-  var quote = "" + json.quoteText + ""
+  var quote = '' + json.quoteText + ''
   $(".quote").html(quote)
   var author = "~" + json.quoteAuthor + "";
   $(".author").html(author)
   })
   })
 })
-
-
-  /*$('.newQuote').on('click', function() {
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
-    $(".quote").html(JSON.stringify(json));
-    })
-  })*/
 
